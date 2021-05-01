54fd301fb263435229c7bf84882d75eb6e4a6c4c
jmdinela <dinelajohnmoren@gmail.com>
2021-05-01 16:24:03 +0800

    added remote mysql connection details

--- dbcon.php
+++ dbcon.php
@@ -1,4 +1,4 @@
 <?php
-$conn = mysqli_connect('remotemysql.com','root','','68h9Tq73Jk') or die(mysqli_error());
+$conn = mysqli_connect('remotemysql.com','68h9Tq73Jk','','68h9Tq73Jk') or die(mysqli_error());
 
 ?>
